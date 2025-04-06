a97fe4155113bdd3b2a8545c184e39e87c9418e0
chirag-bgh <chirag2bsingh@gmail.com>
2025-04-06 17:23:29 +0530

    chore: disbale constraint cuttoff wait

--- crates/rbuilder/src/live_builder/mod.rs
+++ crates/rbuilder/src/live_builder/mod.rs
@@ -312,27 +312,27 @@ where
 
             inc_active_slots();
 
-            // If we have a constraints cuttoff time, we should wait until it passes before
-            match timings.receive_constraints_cuttoff_duration {
-                Some(cuttoff_duration) => {
-                    let time_until_constraints_cuttoff = (time_to_slot + cuttoff_duration)
-                        .saturating_sub(time::Duration::seconds(
-                            SECONDS_PER_SLOT.try_into().unwrap(),
-                        ));
-                    if time_until_constraints_cuttoff.is_negative() {
-                        debug!(
-                            slot = payload.slot(),
-                            "Constraints cuttoff time hasn't passed, sleeping for {}s",
-                            time_until_constraints_cuttoff.as_seconds_f64().abs()
-                        );
-                        tokio::time::sleep(Duration::from_secs_f64(
-                            time_until_constraints_cuttoff.as_seconds_f64().abs(),
-                        ))
-                        .await;
-                    };
-                }
-                None => debug!("No constraints cuttoff time, proceeding with block building"),
-            };
+            // // If we have a constraints cuttoff time, we should wait until it passes before
+            // match timings.receive_constraints_cuttoff_duration {
+            //     Some(cuttoff_duration) => {
+            //         let time_until_constraints_cuttoff = (time_to_slot + cuttoff_duration)
+            //             .saturating_sub(time::Duration::seconds(
+            //                 SECONDS_PER_SLOT.try_into().unwrap(),
+            //             ));
+            //         if time_until_constraints_cuttoff.is_negative() {
+            //             debug!(
+            //                 slot = payload.slot(),
+            //                 "Constraints cuttoff time hasn't passed, sleeping for {}s",
+            //                 time_until_constraints_cuttoff.as_seconds_f64().abs()
+            //             );
+            //             tokio::time::sleep(Duration::from_secs_f64(
+            //                 time_until_constraints_cuttoff.as_seconds_f64().abs(),
+            //             ))
+            //             .await;
+            //         };
+            //     }
+            //     None => debug!("No constraints cuttoff time, proceeding with block building"),
+            // };
 
             let root_hasher =
                 Arc::from(self.provider.root_hasher(payload.parent_block_num_hash())?);
