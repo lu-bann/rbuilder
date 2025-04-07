ce6b4f36962774244e3d93dac52d1d80c295f919
chirag-bgh <chirag2bsingh@gmail.com>
2025-04-08 04:53:47 +0530

    chore: more logs

--- crates/rbuilder/src/primitives/proofs.rs
+++ crates/rbuilder/src/primitives/proofs.rs
@@ -164,6 +164,9 @@ pub fn generate_inclusion_proofs(
         .map_err(|_| ProofError::FailedToGenerateProof)?;
     assert_eq!(root, witness);
     info!("Multi proofs generated");
+    info!("multi proofs leaves {:?}", multi_proof.leaves);
+    info!("multi proofs branch {:?}", multi_proof.branch);
+    info!("multi proofs indices {:?}", multi_proof.indices);
 
     ssz_rs::multiproofs::verify_merkle_multiproof(
         &multi_proof.leaves,
@@ -281,18 +284,22 @@ pub fn verify_inclusion_proofs(
         .into_iter()
         .map(|h| h.as_slice().try_into().unwrap())
         .collect::<Vec<_>>();
+    info!("incluision proof leaves {:?}", leaves);
     let merkle_proofs = proofs
         .merkle_hashes
         .to_vec()
         .iter()
         .map(|h| h.as_slice().try_into().unwrap())
         .collect::<Vec<_>>();
+    info!("incluision proof merkle_proofs {:?}", merkle_proofs);
     let indexes = proofs
         .generalized_indexes
         .to_vec()
         .iter()
         .map(|h| *h as usize)
         .collect::<Vec<_>>();
+    info!("incluision proof indexes {:?}", indexes);
+
     let root = root.as_slice().try_into().expect("Invalid root length");
 
     // Verify the Merkle multiproof against the root
@@ -339,7 +346,7 @@ mod tests {
                 let tx_trimmed = tx_hex.strip_prefix("0x").unwrap_or(tx_hex);
                 let raw_tx = hex::decode(tx_trimmed).expect("Failed to decode hex");
                 let tx = TransactionSigned::decode(&mut raw_tx.as_slice()).unwrap();
-                println!("i: {:?}", tx.hash());
+                println!("{i}: {:?}", tx.hash());
                 i += 1;
                 tx
             })
